1e34c955810598196ab3aec0f43143c19f19c2e4
DoS <doszlmnv@gmail.com>
2022-02-17 15:36:47 +0600

    logs

--- src/main/kotlin/main/logs/LogsScreen.kt
+++ src/main/kotlin/main/logs/LogsScreen.kt
@@ -1,50 +1,45 @@
 package main.logs
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import theme.gray100
-import kotlin.random.Random
 
 @Composable
 fun LogsScreen(component: LogsComponent) {
 
     val colors = List(15) {
-        List(10) {
-            Color(
-                red = Random.nextInt() % 256,
-                green = Random.nextInt() % 256,
-                blue = Random.nextInt() % 256,
-                alpha = 255
-            )
-        }
+        List(48) { it * 30 }
     }
 
     val verticalScrollState = rememberScrollState()
 
     LazyRow(Modifier.fillMaxSize()) {
         items(colors) {
-            Column(
-                Modifier.padding(horizontal = 8.dp).width(240.dp).fillMaxHeight().verticalScroll(verticalScrollState)
-            ) {
-                Spacer(Modifier.height(16.dp))
-                it.forEach {
-                    Box(Modifier.fillMaxWidth().height(80.dp).background(it))
+            Card(Modifier.padding(vertical = 16.dp, horizontal = 8.dp).width(240.dp)) {
+                Box(
+                    Modifier.fillMaxHeight().verticalScroll(verticalScrollState)
+                ) {
+                    Column(Modifier.fillMaxWidth()) {
+                        it.forEach {
+                            if (it % 60 == 0) {
+                                Divider()
+                            }
+                            Box(Modifier.fillMaxWidth().height(40.dp)) {
+                                val hours =
+                                Text(it.toString())
+                            }
+                        }
+                    }
                 }
-                Spacer(Modifier.height(16.dp))
             }
         }
     }
-
 }
