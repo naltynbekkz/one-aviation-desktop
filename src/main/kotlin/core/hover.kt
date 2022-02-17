8018d92d180c3d1e9efabb3a2a12b243d52396b0
DoS <doszlmnv@gmail.com>
2022-02-18 02:51:15 +0600

    logs

--- src/main/kotlin/core/hover.kt
+++ src/main/kotlin/core/hover.kt
@@ -9,6 +9,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.onPointerEvent
 
@@ -20,4 +22,18 @@ fun Modifier.hover(): Modifier = this.composed {
         .background(
             color = MaterialTheme.colors.primary.copy(alpha = if (isHovered) 0.06f else 0f),
         )
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun Modifier.hover(
+    onDraw: DrawScope.() -> Unit
+): Modifier = this.composed {
+    var isHovered by remember { mutableStateOf(false) }
+    this.onPointerEvent(PointerEventType.Enter) { isHovered = true }
+        .onPointerEvent(PointerEventType.Exit) { isHovered = false }
+        .drawBehind {
+            if (isHovered) {
+                onDraw()
+            }
+        }
 }
--- src/main/kotlin/main/logs/LogsScreen.kt
+++ src/main/kotlin/main/logs/LogsScreen.kt
@@ -1,16 +1,22 @@
 package main.logs
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import core.hover
+import theme.gray600
 
 @Composable
 fun LogsScreen(component: LogsComponent) {
@@ -24,22 +30,55 @@ fun LogsScreen(component: LogsComponent) {
     LazyRow(Modifier.fillMaxSize()) {
         items(colors) {
             Card(Modifier.padding(vertical = 16.dp, horizontal = 8.dp).width(240.dp)) {
-                Box(
-                    Modifier.fillMaxHeight().verticalScroll(verticalScrollState)
-                ) {
-                    Column(Modifier.fillMaxWidth()) {
-                        it.forEach {
-                            if (it % 60 == 0) {
-                                Divider()
-                            }
-                            Box(Modifier.fillMaxWidth().height(40.dp)) {
-                                val hours =
-                                Text(it.toString())
+                Column {
+                    Row(Modifier.height(64.dp).fillMaxWidth()) {
+                        Box(
+                            Modifier.padding(8.dp).size(48.dp)
+                                .background(MaterialTheme.colors.gray600, RoundedCornerShape(50))
+                        )
+                        Column(modifier = Modifier.fillMaxHeight(), verticalArrangement = Arrangement.Center) {
+                            Text("FirstName LastName")
+                            Text("Plane")
+                        }
+                    }
+                    Box(
+                        Modifier.fillMaxHeight().verticalScroll(verticalScrollState)
+                    ) {
+                        Column(Modifier.fillMaxWidth()) {
+                            it.forEach {
+                                if (it % 60 == 0) {
+                                    Divider()
+                                }
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(40.dp)
+                                        .hover {
+                                            drawRect(
+                                                color = Color.Green,
+                                                alpha = 0.3f,
+                                            )
+                                        },
+                                ) {
+                                    Text(it.toTime(), Modifier.padding(start = 8.dp, top = 2.dp))
+                                }
                             }
                         }
                     }
                 }
             }
         }
+    }
+}
+
+fun Int.toTime(): String {
+    val hours = this / 60
+    val minutes = this % 60
+    return buildString {
+        if (hours < 10) append("0")
+        append(hours)
+        append(":")
+        if (minutes < 10) append("0")
+        append(minutes)
     }
 }
